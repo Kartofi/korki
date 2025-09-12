cc7115c2b2df3ed725a31fba467447ca36fbe5a4
Kartofi <anatoli7707@gmail.com>
2025-09-13 00:46:22 +0300

    Init

---
+++ src/structs.rs
@@ -0,0 +1,49 @@
+use std::sync::{Arc, Mutex};
+
+pub struct State {
+    pub query: String,
+
+    pub results: Vec<Emoji>,
+
+    pub selected_index: usize,
+
+    pub emojis: Vec<Emoji>,
+
+    pub recent_emojis: Vec<Emoji>,
+
+    pub entries_box: Option<Arc<Mutex<gtk4::Box>>>,
+    pub text_box: Option<Arc<Mutex<gtk4::Text>>>,
+}
+impl State {
+    pub fn new() -> State {
+        State {
+            query: "".to_string(),
+            results: Vec::new(),
+            selected_index: 0,
+
+            emojis: Vec::new(),
+            recent_emojis: Vec::new(),
+
+            entries_box: None,
+            text_box: None
+        }
+    }
+}
+#[derive(Clone, Debug)]
+pub struct Emoji {
+    pub emoji: String,
+
+    pub name: String,
+}
+
+impl Emoji {
+    pub fn new(name: &str, emoji: &str) -> Emoji {
+        Emoji {
+            name: name.to_string(),
+            emoji: emoji.to_string(),
+        }
+    }
+    pub fn as_string(&self) -> String {
+       format!("{} {}", self.emoji, self.name)
+    }
+}
